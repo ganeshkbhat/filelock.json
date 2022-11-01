4631fda2558398764b3f8121a1fc3d44e2c2eded
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-11-01 17:39:44 +0530

    [improvement] [bug] [docs] [feature] Adding minor changes to require files. Adding idnex.js and index.mjs and changing basic file to src/hasher

--- demos/filelock.json.js
+++ demos/filelock.json.js
@@ -1,3 +1,20 @@
+/**
+ * 
+ * Package: filelock.json
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i filelock.json --save
+ * Github: https://github.com/ganeshkbhat/filelock.json
+ * npmjs Link: https://www.npmjs.com/package/filelock.json
+ * File: index.js
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
 const {
     _writeFileLock, _createSHAHash, _readFileLock,
     _createFileLock, _updateFileLockEntry, _deleteFileLockEntry,
